4aad122f75023b684c6045ccc5ccafef0543f66a
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-04-16 23:47:45 +0530

    Rating Component UI

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Filters = () => {
+  const [rate, setRate] = useState(3);
   return (
     <div className="filters">
       <span className="title">Filter Products</span>
@@ -43,7 +45,7 @@ const Filters = () => {
       </span>
       <span>
         <label style={{ paddingRight: 10 }}>Rating :</label>
-        <Rating rating={byRating} style={{ cursor: "pointer" }} />
+        <Rating rating={rate} style={{ cursor: "pointer" }} />
       </span>
       <Button variant="light">Clear Filters</Button>
     </div>
