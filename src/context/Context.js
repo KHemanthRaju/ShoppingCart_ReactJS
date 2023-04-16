dd25d072b23ef6e38b3e862fe9dd594b3563eadc
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-04-16 22:30:05 +0530

    Generating Products JSON

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,6 +1,18 @@
 import { createContext } from "react";
+
+const { faker } = require("@faker-js/faker");
 const Cart = createContext();
 const Context = ({ children }) => {
+  const products = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.abstract(),
+    inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
+    fastDelivery: faker.datatype.boolean(),
+    ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
+  }));
+  console.log(products);
   return <Cart.Provider>{children}</Cart.Provider>;
 };
 
